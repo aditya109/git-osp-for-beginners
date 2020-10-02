1efa2146a311e005de35d9d4323238066e3a895e
Hinal-Srivastava <hinalsri@gmail.com>
2020-10-02 20:19:31 +0530

    20 Prime Numbers

--- Programming/Python/Problem1/DisplayPrimeArrayElements.py
+++ Programming/Python/Problem1/DisplayPrimeArrayElements.py
@@ -1,5 +1,14 @@
-def main ():
-    pass
+#Contributed by @Hinal-Srivastava
 
-if __name__ == "__main__":
-    pass
+import array as arr
+from sympy import *
+
+a = arr.array(int,[])
+print("Enter any 20 elements in the array",end='')
+for i in range(20):
+    value = int(input())
+    a.insert(value)
+
+for i in range(20):    
+    if(isprime(value)==True):
+        print(a[i], " is prime", end='')
