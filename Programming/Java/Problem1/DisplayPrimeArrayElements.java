85cb22a78961e958521dd7dcb6b49ec3f21f0bb1
Rishiraj Purohit <rishirajpurohit@users.noreply.github.com>
2020-10-02 13:50:33 +0200

    Update DisplayPrimeArrayElements.java

--- Programming/Java/Problem1/DisplayPrimeArrayElements.java
+++ Programming/Java/Problem1/DisplayPrimeArrayElements.java
@@ -1,3 +1,52 @@
-public class DisplayPrimeArrayElements {
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Arrays;
 
+class DisplayPrimeArrayElements {
+
+  public static void main(String[] args) {
+    // Get Input
+    ArrayList<Integer> allNums = getInput(20);
+
+    // Filter Numbers by checking for Prime
+    ArrayList<Integer> primes = new ArrayList<Integer>();
+    for(int num: allNums){
+      if(isPrime(num)) {
+        primes.add(num);
+      }
+    }
+
+    // Print Output
+    printResult(primes);
+  }
+
+  public static ArrayList<Integer> getInput(int count){
+    Scanner input = new Scanner(System.in);
+    ArrayList<Integer> allNums = new ArrayList<Integer>();
+    for(int i=0; i<count; i++){
+      System.out.println("Enter number ("+ i +")");
+      int number = input.nextInt();
+      allNums.add(number);
+    }
+    return allNums;
+  }
+
+  public static boolean isPrime(int num){
+    int m=num/2; //only check first half, other half is just inverse
+    if(num==0||num==1){  
+      return false;      
+    }else{  
+      for(int i=2;i<=m;i++){      
+        if(num%i==0){      
+          return false;
+        }      
+      }      
+    }
+    return true;
+  }
+
+  public static void printResult(ArrayList<Integer> primes){
+    String sb = Arrays.toString(primes.toArray());
+    System.out.println("Result: " + sb);
+  }
 }
