4913ca6538864d6b32148a8764558bc5c67b7520
aditya109 <adikid1996@gmail.com>
2020-10-02 20:04:50 +0530

    my first issue; solved #1
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- Programming/Java/Problem1/DisplayPrimeArrayElements.java
+++ Programming/Java/Problem1/DisplayPrimeArrayElements.java
@@ -1,3 +1,26 @@
-public class DisplayPrimeArrayElements {
 
+public class DisplayPrimeArrayElements {
+    public static void main(String[] args) {
+        int n[] = new int[10];
+        int i, j;
+        for (i = 0; i < n.length; i++) {
+            n[i] = (int) (Math.random() * 100);
+        }
+        for (i = 0; i < n.length; i++) {
+            System.out.print(n[i] + " ");
+        }
+        System.out.println();
+
+        for (i = 0; i < n.length; i++) {
+            int f = 0;
+            for (j = 2; j < n[i]; j++) {
+                if (n[i] % j == 0) {
+                    f++;
+                }
+            }
+
+            if (f == 0)
+                System.out.println(" Prime number=" + n[i]);
+        }
+    }
 }
