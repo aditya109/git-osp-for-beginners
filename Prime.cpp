cf010926891fca3bc0163ee8fba2dbaa10025077
Arvind Kumar <59112306+arvind0009@users.noreply.github.com>
2020-10-02 23:44:24 +0530

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 // function to check number is prime or not..
-IsPrime(int n) { 
+ bool IsPrime(int n) { 
 	if(n<2) return false; // if number is less than 2 then it will not prime 
 	
-	for(int i = 2; i<n/2+1; i++)  //loop to check number is prime or not
-		{
+	for(int i = 2; i<=sqrt(n); i++)  //loop to check number is prime or not
+	{
 			if(n%i==0) return false; // we will check if number is divisible by any number from 2 to n/2+1, then it will not prime
-		}
-	return true;
 	}
+	return true;
+}
 
 int main() {
 	
-	int n = 20;
-	int arr[n];
+	int size = 20;
+	int arr[size];
 	
-	for(int i=0; i<n; i++) cin>>arr[i]; 
+	for(int i=0; i<size; i++) cin>>arr[i]; 
 	
-	for(int i=0; i<n; i++)
+	for(int i=0; i<size; i++)
 		{
 			if(IsPrime(arr[i]) == true) 
 				{
