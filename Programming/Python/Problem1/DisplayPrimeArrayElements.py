537f2d0ac2f75bd11e376a28de2341e489fdb343
Akshay Patil <62352546+akshaypatil314@users.noreply.github.com>
2020-10-02 19:56:48 +0530

    Update DisplayPrimeArrayElements.py

--- Programming/Python/Problem1/DisplayPrimeArrayElements.py
+++ Programming/Python/Problem1/DisplayPrimeArrayElements.py
@@ -1,5 +1,25 @@
-def main ():
-    pass
+def main():
 
+    arr=[100,31,89,43,20,11,0]
+
+
+    # prime numbers are greater than 1
+    for i in range(0,len(arr)):
+        if arr[i] > 1:
+       # check for factors
+           for j in range(2,arr[i]):
+               if (arr[i] % j) == 0:
+                   print(arr[i],"is not a prime number")
+                   break
+               else:
+                   print(arr[i],"is a prime number")
+                   break
+       
+    # if input number is less than
+    # or equal to 1, it is not prime
+        else:
+           print(arr[i],"is not a prime number")
+        
+        
 if __name__ == "__main__":
-    pass
+    main()
