d231b18cc92767d34dd124d2f31908e7ac1872f5
Priyam Gupta <priyammgupta@gmail.com>
2020-10-03 02:04:10 +0530

    formatted

--- Programming/Golang/Problem1/DisplayPrimeArrayElements.go
+++ Programming/Golang/Problem1/DisplayPrimeArrayElements.go
@@ -2,33 +2,33 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"math/big"
+	"strconv"
 )
 
-// Take input from user and add to all the previous inputs and then print out the prime numbers
+// Take input from user and add to all the previous inputs
 
 func main() {
 	var arr [20]int64
 	var input string
 
-  fmt.Println("Enter Integer value")
+	fmt.Println("Enter Integer value")
 	for i := 0; i < 20; i++ {
-		
+
 		_, _ = fmt.Scan(&input)
-		
+
 		val, err := strconv.ParseInt(input, 10, 64)
 		if err != nil {
 			fmt.Println("Not a number")
 		} else {
 			arr[i] = val
-			
+
 		}
 	}
-  fmt.Println("Result")
-  for i := 0; i < 20; i++ {
-    if big.NewInt(arr[i]).ProbablyPrime(0) {
-				fmt.Println(arr[i])
-			}
-  }
+	fmt.Println("Result")
+	for i := 0; i < 20; i++ {
+		if big.NewInt(arr[i]).ProbablyPrime(0) {
+			fmt.Println(arr[i])
+		}
+	}
 }
