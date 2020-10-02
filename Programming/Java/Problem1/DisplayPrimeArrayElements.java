653b441d5a8e3f835ba65b715f63f9f1ac1496f8
dhavalmaniyar <logoutdhaval@gmail.com>
2020-10-02 19:26:59 +0530

    DisplayPrimeArrayElements java program added

--- Programming/Java/Problem1/DisplayPrimeArrayElements.java
+++ Programming/Java/Problem1/DisplayPrimeArrayElements.java
@@ -1,3 +1,23 @@
-public class DisplayPrimeArrayElements {
+public static void main(String[] args) {
+    System.out.println("Enter the no. of elemets");
 
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    int[] a = new int[n];
+    System.out.println("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        a[i] = s.nextInt();
+    }
+    for (int i = 0; i < a.length; i++) {
+        boolean isPrime = true;
+        for (int j = 2; j < a[i]; j++) {
+
+            if (a[i] % j == 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime) {
+            System.out.println(a[i]);
+        }
+    }
 }
