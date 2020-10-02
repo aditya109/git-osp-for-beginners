099f8f82832aa3d1a52e1f141daa8076e40bd59f
Mohandas Pai <amazing2196@gmail.com>
2020-10-02 18:45:09 +0530

    Added Code for c++

--- Programming/C++/Problem1/DisplayPrimeArrayElements.cpp
+++ Programming/C++/Problem1/DisplayPrimeArrayElements.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
+int checkPrime(int n) {
+   bool isPrime = true;
+
+   for(int i = 2; i <= n/2; i++) {
+      if (n%i == 0)
+      {
+         isPrime = false;
+         break;
+      }
+   }
+   return isPrime;
+}
+
 int main() {
-    std::cout << "Hello World!";
-    return 0;
+   bool isPrime;
+   int input;
+   cout<<"Enter a number:";
+   cin>>input;
+   for(int n = 2; n < input; n++)
+   {
+       if(checkPrime(n) == true)
+          cout<<n<<" ";
+   }
+   return 0;
 }
