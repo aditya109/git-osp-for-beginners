5fce89f65ea4e2a99a6c145881208d4669205223
Varun Sharma <coolvarunsharma117@gmail.com>
2020-10-05 02:47:28 +0530

    Update CountingTheWordsStartingWithVowels.java
    Minimized the code and reduced memory usage.
    Hope you merge it. :)

--- Programming/Java/Problem3/CountingTheWordsStartingWithVowels.java
+++ Programming/Java/Problem3/CountingTheWordsStartingWithVowels.java
@@ -1,24 +1,18 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class CountingTheWordsStartingWithVowels {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        System.out.println("Enter a sentence");
-        String s = obj.nextLine();
-        s = s + " ";
-        int i, x = 0;
-        String word = "";
-        for (i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == ' ') {
-                char c = Character.toLowerCase(word.charAt(0));
-                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-                    x = x + 1;
-                word = "";
-            } else {
-                word = word + ch;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String s = sc.nextLine().trim();
+		String []a = s.split(" ");
+		int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            char c = a[i].charAt(0);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            	count++;
             }
         }
-        System.out.println("Number of Words starting with vowels=" + x);
+        System.out.println("Number of Words starting with vowels = " + count);
     }
 }
