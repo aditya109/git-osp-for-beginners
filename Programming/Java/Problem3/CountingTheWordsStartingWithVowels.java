73edd1aee67cdb3de2aad7a45980fe773895958c
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2020-10-07 23:12:59 +0530

    Update CountingTheWordsStartingWithVowels.java (#123)
    String concatenation is a costly operation when we run in a loop, It does take O(N^2) time. Also, I've added clean code with modularisation so it will be easily readable.

--- Programming/Java/Problem3/CountingTheWordsStartingWithVowels.java
+++ Programming/Java/Problem3/CountingTheWordsStartingWithVowels.java
@@ -1,24 +1,31 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class CountingTheWordsStartingWithVowels {
-    public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        System.out.println("Enter a sentence");
-        String s = obj.nextLine();
-        s = s + " ";
-        int i, x = 0;
-        String word = "";
-        for (i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == ' ') {
-                char c = Character.toLowerCase(word.charAt(0));
-                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-                    x = x + 1;
-                word = "";
-            } else {
-                word = word + ch;
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String sentence = in.nextLine();
+        int result = getWordsCount(sentence);
+        System.out.println(result);
+    }
+
+    private static int getWordsCount(String sentence) {
+        String[] words = sentence.split(" ");
+        int count = 0;
+        for (String word: words){
+            if(isStartingWithVowel(word)){
+                count++;
             }
         }
-        System.out.println("Number of Words starting with vowels=" + x);
+        return count;
     }
+
+    private static boolean isStartingWithVowel(String word) {
+        char firstLetter = word.toLowerCase().charAt(0);
+        return  firstLetter == 'a' ||
+                firstLetter =='e' ||
+                firstLetter == 'i' ||
+                firstLetter == 'o' ||
+                firstLetter == 'u';
+    }
+
 }
