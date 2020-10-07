ce2b884a783a8600b55f1c636d2a9a7a8b920518
shehani009 <shehanikandy@yahoo.com>
2020-10-07 20:20:48 +0530

    Inheritance

---
+++ Programming/Java/Inheritance.java
@@ -0,0 +1,11 @@
+class Employee{  
+ float salary=40000;  
+}  
+class Programmer extends Employee{  
+ int bonus=10000;  
+ public static void main(String args[]){  
+   Programmer p=new Programmer();  
+   System.out.println("Programmer salary is:"+p.salary);  
+   System.out.println("Bonus of Programmer is:"+p.bonus);  
+}  
+}  
