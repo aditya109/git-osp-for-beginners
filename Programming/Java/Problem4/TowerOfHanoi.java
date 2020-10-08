fef0b3a83f096eb197d0bcaa4140a8c6b6fa54c5
Mownika <41233359+Mownika25@users.noreply.github.com>
2020-10-08 20:18:30 +0530

    Added Tower of Hanoi (#59)

---
+++ Programming/Java/Problem4/TowerOfHanoi.java
@@ -0,0 +1,27 @@
+// Java recursive program to solve tower of hanoi puzzle 
+import java.io.*;
+import java.util.*;
+class TowerOfHanoi
+{ 
+	// Java recursive function to solve tower of hanoi puzzle 
+	static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) 
+	{ 
+		if (n == 1) 
+		{ 
+			System.out.println("Move disk 1 from rod " + from_rod + " to rod " + to_rod); 
+			return; 
+		} 
+		towerOfHanoi(n-1, from_rod, aux_rod, to_rod); 
+		System.out.println("Move disk " + n + " from rod " + from_rod + " to rod " + to_rod); 
+		towerOfHanoi(n-1, aux_rod, to_rod, from_rod); 
+	} 
+	
+	// Driver method 
+	public static void main(String args[]) 
+	{ 
+         Scanner sc= new Scanner(System.in);
+
+		int n = sc.nextInt(); // Number of disks 
+		towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods 
+	} 
+} 
