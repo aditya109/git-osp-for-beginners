0c4af74de36c049caaaefa15478dae683e4f81ac
pasan04 <pasankamburugamuwa1@gmail.com>
2020-10-09 00:16:52 +0530

    added simple fibanaci series

---
+++ Programming/Java/Problem5/SimpleFibonacciSeries.java
@@ -0,0 +1,19 @@
+import java.util.*;   
+
+class SimpleFibonacciSeries
+{  
+   public static void main(String args[])  
+   {  
+        int n = 10, t1 = 0, t2 = 1;
+        System.out.print("First " + n + " terms: ");
+
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " + ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+   }  
+}  
