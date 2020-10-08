32d275d3b5dc1d7e9767c532dd0a69242dd88e11
pankaj892 <31444506+pankaj892@users.noreply.github.com>
2020-10-08 21:15:03 +0530

    Add files via upload (#111)

---
+++ Programming/Python/Problem1/Patterns/Pattern3.py
@@ -0,0 +1,11 @@
+#Contributed by Pankaj
+#Downward asterisk(*) pattern
+rows = 5
+k = 2 * rows - 2
+for i in range(rows, -1, -1):
+    for j in range(k, 0, -1):
+        print(end=" ")
+    k = k + 1
+    for j in range(0, i + 1):
+        print("*", end=" ")
+    print("")
