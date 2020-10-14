56d526ce61776c580f522073344ccfaf28234fe8
Rohan Shenoy <37835789+rohanshenoy96@users.noreply.github.com>
2020-10-14 10:17:34 +0530

    Count digit (#168)
    * Added Count digit solution with efficiency O(1)
    
    * Changed class name
    
    Co-authored-by: Rohan Shenoy <rshenoy@rshenoy-ltm.internal.salesforce.com>

---
+++ Programming/Java/CountDigit O(1)/CountDigit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class CountDigit
+{
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        System.out.println(Integer.toString(num).length());
+    }
+}
