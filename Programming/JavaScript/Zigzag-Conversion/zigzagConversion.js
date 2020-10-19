7c7819e8ee8bcd33a9dadf10484138c8a21b0580
Ishan Avishka <49125943+I3han@users.noreply.github.com>
2020-10-19 07:42:38 +0530

    ZigZag Conversion #210 (#215)
    * ZigZag Conversion #210
    
    * ZigZag Conversion #210
    
    javascript solution
    
    * ZigZag Conversion directory changed

---
+++ Programming/JavaScript/Zigzag-Conversion/zigzagConversion.js
@@ -0,0 +1,45 @@
+function zigzag(s,n){
+
+    let arraySet = [];
+    for(let i =0;i<n;i++){
+        arraySet[i] = []
+    }
+
+    let change = false;
+    let count = 1;
+    for(let i=0;i<s.length;i++){
+        
+        if(!change){
+            arraySet[count -1].push(s.charAt(i))
+        }else{
+            arraySet[n - count -1].push(s.charAt(i))
+        }
+
+        if((count === n && !change) || (count === n-2 && change)){
+            count = 0
+            change = !change;
+        }
+        count++;
+    }
+
+    let convertedString = ""
+    for(let i=0;i<n;i++){
+        for(let j=0;j<arraySet[i].length;j++){
+            convertedString = convertedString + arraySet[i][j];
+        }
+    }
+
+    return convertedString;
+   
+ 
+}
+
+function main() {
+    let s = "PAYPALISHIRING";
+    var n = 4;
+    /* eslint-disable no-console */
+    console.log(zigzag(s,n))
+    /* eslint-enable no-console */
+}
+
+main();
