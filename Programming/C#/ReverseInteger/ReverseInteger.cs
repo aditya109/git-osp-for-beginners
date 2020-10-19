6b19183f654d192cbe5b84c50e8bcc56d29b43c9
Damian Sikora <dsikora85@gmail.com>
2020-10-19 04:14:48 +0200

    Added Reverse integer method for c# (#219)

---
+++ Programming/C#/ReverseInteger/ReverseInteger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace ReverseInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer number:");
+            var number = Console.ReadLine();
+            Console.WriteLine($"Reverse number is: {ReverseInteger(number)}");
+
+            Console.ReadLine();
+        }
+
+        public static string ReverseInteger(string stringNumberWithSign)
+        {
+            var sb = new StringBuilder();
+            var charArray = stringNumberWithSign.Replace("-", string.Empty).ToCharArray();
+            Array.Reverse(charArray);
+            sb.Append(charArray);
+            if(stringNumberWithSign.StartsWith('-'))
+            {
+                sb.Insert(0, '-');
+            }
+            return sb.ToString();
+        }
+    }
+}
