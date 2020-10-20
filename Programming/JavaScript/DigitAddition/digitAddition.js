22ad47e6f65b51ed5c9242e681e4758dc0e14c19
Kay Pea <19981420+ravenblack24@users.noreply.github.com>
2020-10-20 09:55:51 +0100

    added number and nan example inputs

--- Programming/JavaScript/DigitAddition/digitAddition.js
+++ Programming/JavaScript/DigitAddition/digitAddition.js
@@ -6,4 +6,5 @@ var digitAddition = (num) => {
     return num.toString().split("").reduce((acc, item) => parseInt(acc) + parseInt(item));
 }
 
-digitAddition(152);
+digitAddition(152); // returns 8
+digitAddition("not a number"); // returns 0
