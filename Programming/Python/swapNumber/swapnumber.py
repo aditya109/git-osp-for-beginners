ef9bfb6ab97c533c8fca52cecb9ca012cb36024b
Darshit Pandya <64904327+PandyaDeveloper@users.noreply.github.com>
2020-10-23 13:51:01 +0530

    Adding Swap Value Program
    Adding Swap Value Of Variable Without Using Temp Variables

---
+++ Programming/Python/swapNumber/swapnumber.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value od b : "))
+
+a = a+b # sum of both  
+b = a-b # subtracting value b from sum to get the value of a
+a = a-b # subtracting value a from sum to get the value of b
+
+print(a,b)
+
