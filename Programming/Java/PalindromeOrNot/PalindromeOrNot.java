3d2095bd63dee0c7c78e97c00c43dadbc7c57a37
Yashavi Joshi <y0j00zj@homeoffice.wal-mart.com>
2020-10-24 17:10:37 +0530

    changed incorrect file names and added one program for finding primeNumbers

--- Programming/Java/PalindromeOrNot/PalindromeOrNot.java
+++ Programming/Java/PalindromeOrNot/PalindromeOrNot.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Palindrome {
+public class PalindromeOrNot {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
