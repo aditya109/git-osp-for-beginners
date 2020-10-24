645be25ccc21add185db553234e9ea4557bb8ece
Yasas Sandeepa <yasassandeepa007@gmail.com>
2020-10-24 18:30:03 +0530

    Create Animal Class

---
+++ Programming/Java/Zoo/Animal.java
@@ -0,0 +1,11 @@
+
+//This is animal Entity
+public class Animal {
+
+    private String name;
+    private String age;
+    private String gender;
+    private String species; //we can change this to enum in another version
+    private String mostLikedFood;
+
+}
