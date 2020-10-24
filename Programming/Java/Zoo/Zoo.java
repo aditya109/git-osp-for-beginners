d5a3180ad7eeae08941d73a7003de3c0c6fd4283
Yasas Sandeepa <yasassandeepa007@gmail.com>
2020-10-24 18:34:17 +0530

    Zoo class implemented

---
+++ Programming/Java/Zoo/Zoo.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+// This is our Zoo
+public class Zoo {
+
+    private List<Animal> animalList;
+
+    public List<Animal> getAnimalList() {
+        return animalList;
+    }
+
+    public void setAnimalList(List<Animal> animalList) {
+        this.animalList = animalList;
+    }
+
+}
