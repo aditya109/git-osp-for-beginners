c56fd23e1433e4e267c294b95f9c8b6d60d76f0d
Yasas Sandeepa <yasassandeepa007@gmail.com>
2020-10-24 19:46:18 +0530

    Change the logic in Zoo class

--- Programming/Java/Zoo/Zoo.java
+++ Programming/Java/Zoo/Zoo.java
@@ -1,20 +1,40 @@
-import java.util.List;
-
 // This is our Zoo
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Zoo {
 
-    private List<Animal> animalList;
+    private LinkedList<Animal> animalList;
 
-    public List<Animal> getAnimalList() {
+    public Zoo() {
+        this.animalList = new LinkedList<Animal>();
+    }
+
+    public LinkedList<Animal> getAnimalList() {
         return animalList;
     }
 
-    public void setAnimalList(List<Animal> animalList) {
-        this.animalList = animalList;
+    public void addAnimal(Animal animal) {
+        this.getAnimalList().add(animal);
+
     }
 
+    // todo: adding proper logic to get the animal
     public void feedAnimal(String name) {
         System.out.println("You are Feeding to " + name);
+    }
+
+    public void viewAnimals() {
+        System.out.println("==========All animals in the ZOO===========");
+        LinkedList<Animal> list = this.getAnimalList();
+        Iterator<Animal> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        System.out.println("============================================");
+        System.out.println("Thank you!");
+
     }
 
 
