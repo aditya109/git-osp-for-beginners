c7a7ded3aeefbeed20fd36c14995f1026a110f70
Liad Yosef <liady@users.noreply.github.com>
2020-10-27 20:00:36 +0200

    lint fixes

--- Programming/JavaScript/Zigzag-Conversion/zigzagConversion.js
+++ Programming/JavaScript/Zigzag-Conversion/zigzagConversion.js
@@ -1,45 +1,41 @@
-function zigzag(s,n){
+function zigzag(s, n) {
+  const arraySet = [];
+  for (let i = 0; i < n; i++) {
+    arraySet[i] = [];
+  }
 
-    let arraySet = [];
-    for(let i =0;i<n;i++){
-        arraySet[i] = []
+  let change = false;
+  let count = 1;
+  for (let i = 0; i < s.length; i++) {
+    if (!change) {
+      arraySet[count - 1].push(s.charAt(i));
+    } else {
+      arraySet[n - count - 1].push(s.charAt(i));
     }
 
-    let change = false;
-    let count = 1;
-    for(let i=0;i<s.length;i++){
-        
-        if(!change){
-            arraySet[count -1].push(s.charAt(i))
-        }else{
-            arraySet[n - count -1].push(s.charAt(i))
-        }
-
-        if((count === n && !change) || (count === n-2 && change)){
-            count = 0
-            change = !change;
-        }
-        count++;
+    if ((count === n && !change) || (count === n - 2 && change)) {
+      count = 0;
+      change = !change;
     }
+    count++;
+  }
 
-    let convertedString = ""
-    for(let i=0;i<n;i++){
-        for(let j=0;j<arraySet[i].length;j++){
-            convertedString = convertedString + arraySet[i][j];
-        }
+  let convertedString = "";
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < arraySet[i].length; j++) {
+      convertedString = convertedString + arraySet[i][j];
     }
+  }
 
-    return convertedString;
-   
- 
+  return convertedString;
 }
 
 function main() {
-    let s = "PAYPALISHIRING";
-    var n = 4;
-    /* eslint-disable no-console */
-    console.log(zigzag(s,n))
-    /* eslint-enable no-console */
+  let s = "WE_ARE_HIRING";
+  var n = 4;
+  /* eslint-disable no-console */
+  console.log(zigzag(s, n));
+  /* eslint-enable no-console */
 }
 
 main();
