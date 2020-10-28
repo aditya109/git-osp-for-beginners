7f660552d636bb9ef0023e951e293d28ca85b29f
Yashasvi Joshi <yashasvi.joshi@cumminscollege.in>
2020-10-28 09:01:17 +0530

    corrected issue: In one single java program 3 classes were used. (#302)
    * corrected issue: In one single java program 3 classes were used
    
    * two different folders for same kind of program :corrected
    
    * incorrect package name removed
    
    Co-authored-by: Yashavi Joshi <y0j00zj@homeoffice.wal-mart.com>

--- Programming/Java/AddTwoNumbers/AddTwoNumber.java
+++ Programming/Java/AddTwoNumbers/AddTwoNumber.java
@@ -1,63 +1,59 @@
-package test;
-
 import java.util.Scanner;
 
 public class AddTwoNumber {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter length for 1st list:");
-		int length = sc.nextInt();
-		Node first = init(length, sc);
-		
-		System.out.println("Enter length for 2nd list:");
-		length = sc.nextInt();
-		Node second = init(length, sc);
-		
-		sc.close();
-		
-		Solution solution = new Solution();
-        Node res = solution.addLists(first, second);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter length for 1st list:");
+        int length = sc.nextInt();
+        System.out.println("Enter numbers for 1st list:");
+        Node first = init(length, sc);
+
+        System.out.println("Enter length for 2nd list:");
+        length = sc.nextInt();
+        System.out.println("Enter numbers for 2nd list:");
+        Node second = init(length, sc);
+
+        sc.close();
+
+        Solution solution = new Solution();
+        Node res = addLists(first, second);
         printList(res);
-		
-	}
-	
-	static Node init(int length, Scanner sc) {
-		int val;
-		Node node = null;
-		Node tail = node;
-		for(int i=0;i<length;i++) {
-			val = sc.nextInt();
-			if(node == null) {
-				node = new Node(val);
-				tail = node;
-			} else {
-				tail.next = new Node(val);
-				tail = tail.next;
-			}
-		}
-		
-		return node;
-	}
-	
-	static void printList(Node n){
-        while(n!=null){
-            System.out.print(n.data+" ");
+    }
+
+    static Node init(int length, Scanner sc) {
+        int val;
+        Node node = null;
+        Node tail = node;
+        for (int i = 0; i < length; i++) {
+            val = sc.nextInt();
+            if (node == null) {
+                node = new Node(val);
+                tail = node;
+            } else {
+                tail.next = new Node(val);
+                tail = tail.next;
+            }
+        }
+
+        return node;
+    }
+
+    static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
             n = n.next;
         }
         System.out.println();
     }
-}
 
-
-class Solution{
-    static Node addLists(Node first, Node second){
+    public static Node addLists(Node first, Node second) {
         first = reverse(first);
         second = reverse(second);
         int carry = 0;
         Node prev = null;
-        while(first != null || second != null){
+        while (first != null || second != null) {
             int sum = carry + (first != null ? first.data : 0) + (second != null ? second.data : 0);
             carry = sum / 10;
             sum = sum % 10;
@@ -66,22 +62,22 @@ class Solution{
             Node node = new Node(sum);
             node.next = prev;
             prev = node;
-            
+
         }
-        if(carry > 0){
+        if (carry > 0) {
             Node node = new Node(carry);
             node.next = prev;
             prev = node;
         }
-        
+
         return reverse(prev);
     }
-    
-    static Node reverse(Node node){
+
+    static Node reverse(Node node) {
         Node prev = null;
         Node next = null;
         Node current = node;
-        while(current != null){
+        while (current != null) {
             next = current.next;
             current.next = prev;
             prev = current;
@@ -89,14 +85,14 @@ class Solution{
         }
         return prev;
     }
-}
 
-class Node {
-    int data;
-    Node next;
+    static class Node {
+        int data;
+        Node next;
 
-    Node(int d) {
-        data = d;
-        next = null;
+        Node(int d) {
+            data = d;
+            next = null;
+        }
     }
 }
