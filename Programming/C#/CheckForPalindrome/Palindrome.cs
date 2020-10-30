9f532f6fe62073143e9230526920819f0d5d29c1
Calin George-Adrian <calingeorgeadrian98@gmail.com>
2020-10-31 00:02:32 +0200

    Added C# solution for Palindrome check

---
+++ Programming/C#/CheckForPalindrome/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            int number, r, sum = 0, temp;
+
+            Console.Write("Enter the number: ");
+            number = int.Parse(Console.ReadLine());
+            temp = number;
+            Console.WriteLine();
+
+            while (number > 0)
+            {
+                r = number % 10;
+                sum = (sum * 10) + r;
+                number = number / 10;
+            }
+
+            if (temp == sum)
+                Console.Write("Number is a palindrome.");
+            else
+                Console.WriteLine("Number is not a palindrome");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
