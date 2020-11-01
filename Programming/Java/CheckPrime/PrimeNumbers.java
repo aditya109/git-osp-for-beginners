70a3f0e8ef352d093e5f87ba86bc9d659c87fb73
Hirusha Chamod <49561209+hiru997@users.noreply.github.com>
2020-11-01 12:37:17 +0530

    Java Add Two Numbers (#360)
    Added scanner api

--- Programming/Java/CheckPrime/PrimeNumbers.java
+++ Programming/Java/CheckPrime/PrimeNumbers.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
 
 public class PrimeNumbers {
-        public static void main(String args[])
+        public static void main(String []args)
         {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter a number till which you want to find to all prime numbers smaller than or equal your number");
+          try(Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a number till which you want to find to all prime numbers smaller than or equal your number : ");
             int n = sc.nextInt();
-            System.out.print("all the prime numbers smaller than or equal to " + n);
+            System.out.print("all the prime numbers smaller than or equal to " + n + ":");
             PrimeNumbers p = new PrimeNumbers();
             p.findNumbers(n);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            }   
         }
+
         private void findNumbers(int n) {
 
-            boolean prime[] = new boolean[n+1];
+            boolean[] prime = new boolean[n+1];
             for(int i=0;i<n;i++)
                 prime[i] = true;
 
             for(int j = 2; j*j <=n; j++)
             {
-                if(prime[j] == true)
+                if(!(prime[j]))
                 {
                     for(int i = j*j; i <= n; i += j)
                         prime[i] = false;
@@ -26,7 +30,7 @@ public class PrimeNumbers {
             }
             for(int i = 2; i <= n; i++)
             {
-                if(prime[i] == true)
+                if(!(prime[i]))
                     System.out.print(i + " ");
             }
 
