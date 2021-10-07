b8b5cb1ae151a4d6f2d7f76a81f697508a9ea97f
b s <bs@bs-MacBook-Pro.local>
2021-10-07 14:48:59 +0530

    added ReverseString

---
+++ Programming/Java/ReverseString/ReverseString.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ReverseNumber{
+    public static void main(String[] args){
+        String input = "Alpabets";
+        StringBuilder input1 = new StringBuilder();
+        // append a string into StringBuilder input1
+        input1.append(input);
+        // reverse StringBuilder input1
+        input1.reverse();
+        // print reversed String
+        System.out.println(input1);
+        }
+    }
