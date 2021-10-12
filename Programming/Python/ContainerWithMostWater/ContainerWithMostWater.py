147edc157eb95082959aff161e420f469111e7de
Pranjal Goyal <pranjalgoyal13@gmail.com>
2021-10-12 21:46:09 +0530

    Added python solution for ContainerWIthMostWater problem (#537)
    * Added Soluion of ContainweWithMostWater problem
    
    * Added Example
    
    Signed-off-by: Pranjal Goyal <pranjalgoyal13@gmail.com>

---
+++ Programming/Python/ContainerWithMostWater/ContainerWithMostWater.py
@@ -0,0 +1,38 @@
+"""
+Question: Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). 
+Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+Given Example
+Example 1:
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+"""
+
+
+def max_capacity(height):
+    max_area = 0
+        
+    i = 0
+    j = len(height) - 1
+    
+    # Using the two pointers namely i and j
+    while i < j:
+        curr_area = (j - i) * min(height[i], height[j])
+        max_area = max(max_area, curr_area)
+        if height[i] <= height[j]:
+            i = i + 1
+        else:
+            j = j - 1
+    return max_area
+
+def main():
+    height = list(map(int,input().split(" ")))
+    print(max_capacity(height))
+
+
+if __name__ == '__main__':
+    main()
+    
+#Time Complexity = O(n)
+#Space Complexity = O(1)
