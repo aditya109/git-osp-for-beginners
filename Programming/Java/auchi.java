ee58ce8f4deb62568ef76ca84ee031b7f4f687f5
auchitya <54791078+auchitya@users.noreply.github.com>
2021-10-14 02:10:45 +0530

    Added  bla bla algo
    jdjdkdj

---
+++ Programming/Java/auchi.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int j=0;j<n/2;j++){
+            for(int i=0;i<n;i++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=temp;
+            }
+        }
+    }
+}
